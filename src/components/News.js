9dba0567b2a73693723391c6cc4cc49779cabd29
AnuragShaw <anuragshaw298@gmail.com>
2024-06-07 14:39:40 +0530

    Initial Commit

---
+++ src/components/News.js
@@ -0,0 +1,101 @@
+import React, { useEffect, useState } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
+
+
+const News = (props) => {
+  const [articles, setArticles] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+  const [totalResults, setTotalResults] = useState(0)
+  // document.title = `${capitalizeFirstLetter(props.category)} - NewsApp`;
+
+  const capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+
+  const updateNews = async (pageNo) => {
+    props.setProgress(10);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(40);
+    let parsedData = await data.json();
+    props.setProgress(70);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    props.setProgress(100);
+
+  }
+
+  useEffect(() => {
+    updateNews();
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsApp`;
+  }, [])
+
+  // handlePrevClick = async ()=>{
+  //   setPage(page-1);
+  //   updateNews();
+  // }
+  // handleNextClick = async ()=>{
+  //   setPage(page+1);
+  //   updateNews();
+  // }
+
+  const fetchMoreData = async () => {
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+    setPage(page+1);
+    let data = await fetch(url);
+    let parsedData = await data.json();
+    setArticles(articles.concat(parsedData.articles));
+    setTotalResults(parsedData.totalResults);
+    setLoading(false);
+    
+  }
+
+  return (
+    <>
+      <h1 className="text-center" style={{ margin: '30px 0', marginTop: '70px' }}>NewsApp - Top Headlines - {capitalizeFirstLetter(props.category)}</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+
+        <div className="container">
+          <div className="row">
+            {articles.map((element, index) => {
+              return <div className="col-md-4" key={index}>
+                <NewsItem title={element.title} description={element.description ? element.description.slice(0, 150) : ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
+          </div>
+        </div>
+
+      </InfiniteScroll>
+      {/* <div className="container d-flex justify-content-between">
+          <button disabled={page<=1} type="button" className="btn btn-dark" onClick={handlePrevClick}>&larr; Previous</button>
+          <button disabled={page+1 > Math.ceil(totalResults/props.pageSize)} type="button" className="btn btn-dark" onClick={handleNextClick}>Next &rarr;</button>
+        </div> */}
+    </>
+  )
+}
+
+
+News.defaultProps = {
+  country: 'in',
+  pageSize: 9,
+  category: 'general'
+}
+News.propTypes = {
+  name: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+}
+export default News
---
+++ src/components/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+const Spinner = () => {
+
+  return (
+    <div className="text-center">
+      <img className='my-4' src="http://samherbert.net/svg-loaders/svg-loaders/audio.svg" alt="" style={{ filter: "invert(1)", width: "100" }} />
+    </div>
+  )
+}
+
+export default Spinner
